21e00b7a446f274d68be1a80ca2732bd17b3a974
Shahzaib1111111 <shahzaibtrafix@gmail.com>
2024-11-21 17:19:16 +0500

    first commit

---
+++ IServiceCalc.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceModel;
+
+namespace WCFSample
+{
+    [ServiceContract]
+    public interface IServiceCalc
+    {
+
+        [OperationContract]
+        int Sum(int num1, int num2);
+
+        [OperationContract]
+        int Subtract(int num1, int num2);
+
+        [OperationContract]
+        int Multiply(int num1, int num2);
+
+        [OperationContract]
+        int Divide(int num1, int num2);
+
+    }
+}
