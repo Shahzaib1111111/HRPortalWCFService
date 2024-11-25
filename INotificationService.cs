f136d94673258acc8d893a9423dac67c941ccc48
Shahzaib1111111 <shahzaibtrafix@gmail.com>
2024-11-25 21:08:23 +0500

    [add] dll references

--- INotificationService.cs
+++ INotificationService.cs
@@ -8,7 +8,6 @@ namespace WCFService
     [ServiceContract]
     public interface INotificationService
     {
-
         [OperationContract]
         Task OnSalaryDisbursed(List<Employee> employees);
     }
