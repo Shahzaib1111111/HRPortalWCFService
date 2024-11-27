22d26a0c4ec06180ef885b2e89c41a77a722904a
Shahzaib Zafar <szafar@trafix.com>
2024-11-27 13:16:31 +0500

    added ex handling

--- NotificationService.cs
+++ NotificationService.cs
@@ -12,22 +12,29 @@ namespace WCFService
         private static readonly object m_logLock = new object();
         public string OnSalaryDisbursed(List<Employee> employees)
         {
-            int noOfThreads = 4;
-            int noOfEmployeesToBeProcessedByEachThread = employees.Count() / noOfThreads;
-            List<Task> tasks = new List<Task>();
-            for (int i = 0; i < noOfThreads; i++)
+            try
             {
-                //DRY RUN
-                //i=0, noOfEmployeesToBeProcessedByEachThread=3
-                //0=> skips 0 employees and take 3 starting from 0
-                //1=> skips 3 employees and take 3 starting from 3
-                //2=> skips 6 employees and take 3 starting from 6
-                List<Employee> batchOfEmployees = employees.Skip(i * noOfEmployeesToBeProcessedByEachThread).Take(noOfEmployeesToBeProcessedByEachThread).ToList();
-                tasks.Add(Task.Run(() => SendEmail(batchOfEmployees)));
+                int noOfThreads = 4;
+                int noOfEmployeesToBeProcessedByEachThread = employees.Count() / noOfThreads;
+                List<Task> tasks = new List<Task>();
+                for (int i = 0; i < noOfThreads; i++)
+                {
+                    //DRY RUN
+                    //i=0, noOfEmployeesToBeProcessedByEachThread=3
+                    //0=> skips 0 employees and take 3 starting from 0
+                    //1=> skips 3 employees and take 3 starting from 3
+                    //2=> skips 6 employees and take 3 starting from 6
+                    List<Employee> batchOfEmployees = employees.Skip(i * noOfEmployeesToBeProcessedByEachThread).Take(noOfEmployeesToBeProcessedByEachThread).ToList();
+                    tasks.Add(Task.Run(() => SendEmail(batchOfEmployees)));
+                }
+                Task.WhenAll(tasks);
             }
-            Task.WhenAll(tasks);
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ex.Message;
+            }
             return "Email has been sent to all employees";
-
         }
 
         private static void SendEmail(object state)
